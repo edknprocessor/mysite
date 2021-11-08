c6222b8f0d51331c1f1310fc80415f36fc27e1dc
edoknprocessor <edknprocessor@users.noreply.github.com>
2021-11-08 20:12:24 +0900

    refactoring

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,22 @@
 import * as React from "react"
 import tw, { styled } from "twin.macro"
+import Post from "../components/post"
+import Img from "gatsby-image"
 
-const Card = ({ children }) => (
+const Card = (props) => (
   <div css={[
     tw`w-full p-8 bg-white shadow-xl rounded-lg`
   ]}>
-    {children}
+    <p css={[tw`text-2xl`]}>{props.title}</p>
+    <p css={[tw`text-gray-500`]}>{props.date}</p>
+    <Img
+      alt={props.title}
+      fluid={props.img}
+      css={[tw`my-8`]}
+    />
+    <Post
+      html={props.html}
+    />
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,6 @@ import { StaticImage } from "gatsby-plugin-image"
 import { graphql } from 'gatsby'
 import Seo from "../components/seo"
 import Card from "../components/card"
-import Post from "../components/post"
-import Img from "gatsby-image"
 
 const IndexPage = ({ data }) => {
   return (
@@ -27,18 +25,11 @@ const IndexPage = ({ data }) => {
           <p css={[tw`text-center text-3xl`]}>作品リスト</p>
           {data.allMarkdownRemark.nodes.map( node => (
             <div css={[tw`mt-8`]} key={node.frontmatter.title}>
-              <Card>
-                <p css={[tw`text-2xl`]}>{node.frontmatter.title}</p>
-                <p css={[tw`text-gray-500`]}>{node.frontmatter.date}</p>
-                <Img
-                  alt={node.frontmatter.title}
-                  fluid={node.frontmatter.featuredImage.childImageSharp.fluid}
-                  css={[tw`my-8`]}
-                />
-                <Post
-                  html={node.html}
-                />
-              </Card>
+              <Card title={node.frontmatter.title}
+                date={node.frontmatter.date}
+                img={node.frontmatter.featuredImage.childImageSharp.fluid}
+                html={node.html}
+              />
             </div>
           ))}
         </div>
