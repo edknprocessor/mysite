470e856dab0b61cd5af954b31b82afa48df06563
edoknprocessor <edknprocessor@users.noreply.github.com>
2022-01-30 15:24:13 +0900

    modify pagination

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -3,10 +3,22 @@ import tw from "twin.macro"
 import { Link } from "gatsby"
 
 const Pagination = (props) => {
+  console.log(props.pre)
+  console.log(props.next)
+
+  const hasNotPrePage = props.pre === ""
+  const hasNotNextPage = props.next === ""
+
   return(
     <ul css={[tw`flex py-4 justify-center`]}>
-      <li css={[tw`px-4`]}><Link to={props.pre}>前の10件←</Link></li>
-      <li css={[tw`px-4`]}><Link to={props.next}>→次の10件</Link></li>
+      {
+        !hasNotPrePage &&
+          <li css={[tw`px-4`]}><Link to={props.pre}>戻る←</Link></li>
+      }
+      {
+        !hasNotNextPage &&
+        <li css={[tw`px-4`]}><Link to={props.next}>→次へ</Link></li>
+      }
     </ul>
   )
 }
